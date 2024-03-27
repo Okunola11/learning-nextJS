3191ca564ab318400519f7715715b24f2b975147
Okunola Abdulwasiu <abdayoade@gmail.com>
2024-03-27 15:01:50 +0100

    Added usersService

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,12 +8,15 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get() // GET /users or /users?role=value&age=42 (those are query parameters)
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
+    return this.usersService.findAll(role);
   }
 
   /*  @Get('interns') // GET /users/interns. We dont need this, it's just to show how to order routes.
@@ -21,23 +24,38 @@ export class UsersController {
     return [];
   } */
 
-  @Get(':id') // GET /users/:id
+  @Get(':id') // GET /users/:id. Params are always string
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.usersService.findOne(+id); // can use parseInt() or unary plus(+) which was used as id expects number
   }
 
   @Post() // POST /users
-  create(@Body() user: {}) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.create(user);
   }
 
   @Patch(':id') // PATCH /users/:id
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  update(
+    @Param('id') id: string,
+    @Body()
+    userUpdate: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.update(+id, userUpdate);
   }
 
   @Delete(':id') // DELETE /users/:id
   delete(@Param('id') id: string) {
-    return { id };
+    return this.usersService.delete(+id);
   }
 }
