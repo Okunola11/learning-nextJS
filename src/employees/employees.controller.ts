03ad85f0ed882c6fdb4cc8fdbf8ba54c72d2350e
Okunola Abdulwasiu <abdayoade@gmail.com>
2024-03-29 07:23:07 +0100

    Implemented logging, rate limit and some other functionalities

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -7,24 +7,37 @@ import {
   Param,
   Delete,
   Query,
+  Ip,
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from '@prisma/client';
-
+import { Throttle, SkipThrottle } from '@nestjs/throttler';
+import { MyLoggerService } from 'src/my-logger/my-logger.service';
+@SkipThrottle() // this will skip for everything in the controller
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
+  private readonly logger = new MyLoggerService(EmployeesController.name);
 
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
     return this.employeesService.create(createEmployeeDto);
   }
 
+  @SkipThrottle({ default: false }) // this will rate limit this particular GET request
   @Get()
-  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+  findAll(
+    @Ip() ip: string,
+    @Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN',
+  ) {
+    this.logger.log(
+      `Request for all Employees\t${ip}`,
+      EmployeesController.name,
+    );
     return this.employeesService.findAll(role);
   }
 
+  @Throttle({ short: { ttl: 1000, limit: 1 } }) // overriding what was set @app.module. If no name, use default
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.employeesService.findOne(+id);
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,5 +5,6 @@ import { UsersService } from './users.service';
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService], // so it will be visible in auth
 })
 export class UsersModule {}
