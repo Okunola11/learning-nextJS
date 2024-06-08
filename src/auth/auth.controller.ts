ec3fde758cb4083bb37c7ba0a708ee3257e7c69d
Okunola Abdulwasiu <abdayoade@gmail.com>
2024-06-08 20:31:12 +0100

    Added signIn dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,22 +7,23 @@ import {
   Get,
   Request,
   UseGuards,
-} from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { AuthGuard } from "./auth.guard";
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+import { SignInDto } from './dto/signIn.dto';
 
-@Controller("auth")
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
-  @Post("login")
-  signIn(@Body() signInDto: Record<string, any>) {
+  @Post('login')
+  signIn(@Body() signInDto: SignInDto /* Record<string, any> */) {
     return this.authService.signIn(signInDto.username, signInDto.password);
   }
 
   @UseGuards(AuthGuard)
-  @Get("profile")
+  @Get('profile')
   getProfile(@Request() req) {
     return req.user;
   }
